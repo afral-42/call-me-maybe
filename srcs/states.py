5c1e0616251aecafb4a3c008b5e3d327db34b654
Anselme Bounouar <abounoua@student.42lyon.fr>
2026-04-20 22:07:00 +0200

    feat: implement robust FSM for JSON numbers, dynamic strings and booleans

--- srcs/states.py
+++ srcs/states.py
@@ -49,88 +49,215 @@ class StaticStringState(State):
 
 
 class NumberMachineState(Enum):
+    ZERO = auto()
     SIGN = auto()
-    START = auto()
+    START_MINUS = auto()
     INTEGRAL = auto()
     POINT = auto()
     FRACTIONAL = auto()
+    EXPONENT = auto()
+    EXPONENT_SIGN = auto()
+    EXPONENT_INTEGRAL = auto()
 
 
 class NumberCharType(Enum):
-    DIGIT_WITH_ZERO = "0123456789"
-    DIGIT = "123456789"
+    DIGITS = "123456789"
     POINT = "."
     END = ","
     MINUS = "-"
     ZERO = "0"
-    INVALID = "!"
+    EXPONENT = "eE"
+    PLUS = "+"
 
 
 class NumberState(State):
-    def __init__(self, trie: Trie) -> None:
+    def __init__(self, end: str) -> None:
         self.done = False
         self.state = NumberMachineState.SIGN
 
-        self.authorized = {
-            NumberMachineState.SIGN: trie.constrained_search("-0123456789"),
-            NumberMachineState.START: trie.constrained_search("0123456789"),
-            NumberMachineState.INTEGRAL: trie.constrained_search("0123456789."),  # Attention car l'arbre peut tres bien renvoyer .. si prefix! (ajouter un argument "unique")
-            NumberMachineState.POINT: trie.constrained_search("0123456789"),
-            NumberMachineState.FRACTIONAL: trie.constrained_search("0123456789")
+        self.end = end
+
+        self.search_map = {
+            NumberMachineState.SIGN: "-0123456789",
+            NumberMachineState.ZERO: f".{end}",
+            NumberMachineState.START_MINUS: "0123456789",
+            NumberMachineState.INTEGRAL: f"0123456789.{end}eE",  # Attention car l'arbre peut tres bien renvoyer .. si prefix! (ajouter un argument "unique")
+            NumberMachineState.POINT: "0123456789",
+            NumberMachineState.FRACTIONAL: f"0123456789{end}eE",
+            NumberMachineState.EXPONENT: "-+0123456789",
+            NumberMachineState.EXPONENT_SIGN: "0123456789",
+            NumberMachineState.EXPONENT_INTEGRAL: f"0123456789{end}"
         }
 
         self.transitions = {
             (NumberMachineState.SIGN, NumberCharType.MINUS): (
-                NumberMachineState.START
+                NumberMachineState.START_MINUS
             ),
-            (NumberMachineState.SIGN, NumberCharType.DIGIT): (
+            (NumberMachineState.SIGN, NumberCharType.DIGITS): (
                 NumberMachineState.INTEGRAL
             ),
             (NumberMachineState.SIGN, NumberCharType.ZERO): (
+                NumberMachineState.ZERO
+            ),
+            (NumberMachineState.START_MINUS, NumberCharType.ZERO): (
+                NumberMachineState.ZERO
+            ),
+            (NumberMachineState.START_MINUS, NumberCharType.DIGITS): (
+                NumberMachineState.INTEGRAL
+            ),
+            (NumberMachineState.ZERO, NumberCharType.POINT): (
                 NumberMachineState.POINT
             ),
-            (NumberMachineState.INTEGRAL, NumberCharType.DIGIT_WITH_ZERO): (
+            (NumberMachineState.INTEGRAL, NumberCharType.DIGITS): (
+                NumberMachineState.INTEGRAL
+            ),
+            (NumberMachineState.INTEGRAL, NumberCharType.ZERO): (
                 NumberMachineState.INTEGRAL
             ),
             (NumberMachineState.INTEGRAL, NumberCharType.POINT): (
                 NumberMachineState.POINT
             ),
-            (NumberMachineState.POINT, NumberCharType.DIGIT_WITH_ZERO): (
+            (NumberMachineState.POINT, NumberCharType.DIGITS): (
                 NumberMachineState.FRACTIONAL
+            ),
+            (NumberMachineState.POINT, NumberCharType.ZERO): (
+                NumberMachineState.FRACTIONAL
+            ),
+            (NumberMachineState.FRACTIONAL, NumberCharType.ZERO): (
+                NumberMachineState.FRACTIONAL
+            ),
+            (NumberMachineState.FRACTIONAL, NumberCharType.DIGITS): (
+                NumberMachineState.FRACTIONAL
+            ),
+            (NumberMachineState.INTEGRAL, NumberCharType.EXPONENT): (
+                NumberMachineState.EXPONENT
+            ),
+            (NumberMachineState.FRACTIONAL, NumberCharType.EXPONENT): (
+                NumberMachineState.EXPONENT
+            ),
+            (NumberMachineState.EXPONENT, NumberCharType.MINUS): (
+                NumberMachineState.EXPONENT_SIGN
+            ),
+            (NumberMachineState.EXPONENT, NumberCharType.PLUS): (
+                NumberMachineState.EXPONENT_SIGN
+            ),
+            (NumberMachineState.EXPONENT, NumberCharType.DIGITS): (
+                NumberMachineState.EXPONENT_INTEGRAL
+            ),
+            (NumberMachineState.EXPONENT, NumberCharType.ZERO): (
+                NumberMachineState.EXPONENT_INTEGRAL
+            ),
+            (NumberMachineState.EXPONENT_SIGN, NumberCharType.DIGITS): (
+                NumberMachineState.EXPONENT_INTEGRAL
+            ),
+            (NumberMachineState.EXPONENT_SIGN, NumberCharType.ZERO): (
+                NumberMachineState.EXPONENT_INTEGRAL
+            ),
+            (NumberMachineState.EXPONENT_INTEGRAL, NumberCharType.DIGITS): (
+                NumberMachineState.EXPONENT_INTEGRAL
+            ),
+            (NumberMachineState.EXPONENT_INTEGRAL, NumberCharType.ZERO): (
+                NumberMachineState.EXPONENT_INTEGRAL
             ),
         }
 
-    def _get_char_type(self, char: str) -> NumberCharType:
+    def _get_char_type(self, char: str) -> NumberCharType | str:
+        if char == self.end:
+            return self.end
+
         for key in NumberCharType:
             if char in key.value:
                 return key
 
-        return NumberCharType.INVALID
+        raise StateException
 
     def consume(self, text: str) -> str:
         for i, char in enumerate(text):
             char_type = self._get_char_type(char)
 
             match char_type:
-                case NumberCharType.INVALID:
-                    raise StateException
-                case NumberCharType.END:
+                case self.end:
                     self.done = True
                     return text[i:]
                 case _:
-                    self.state = self.transitions[(self.state, char_type)]
+                    try:
+                        self.state = self.transitions[(self.state, char_type)]
+                    except KeyError:
+                        raise StateException
 
         return ""
 
     def get_valid_tokens(self, trie: Trie) -> set[int]:
-        return self.authorized[self.state]
+        return trie.constrained_search(self.search_map[self.state])
 
     def is_done(self) -> bool:
         return self.done
 
 
+class StringMachineState(Enum):
+    ESCAPE = auto()
+    NORMAL = auto()
+
+
+class StringCharType(Enum):
+    ESCAPE_FLAG = "\\"
+    OTHER = ""
+    END = "\""
+
+
 class DynamicStringState(State):
-    pass
+    def __init__(self) -> None:
+        self.state = StringMachineState.NORMAL
+        self.done = False
+
+        self.transitions = {
+            (StringMachineState.ESCAPE, StringCharType.OTHER): (
+                StringMachineState.NORMAL
+            ),
+            (StringMachineState.NORMAL, StringCharType.OTHER): (
+                StringMachineState.NORMAL
+            ),
+            (StringMachineState.NORMAL, StringCharType.ESCAPE_FLAG): (
+                StringMachineState.ESCAPE
+            ),
+            (StringMachineState.ESCAPE, StringCharType.ESCAPE_FLAG): (
+                StringMachineState.NORMAL
+            ),
+        }
+
+    def _get_char_type(self, char: str) -> StringCharType:
+        for key in StringCharType:
+            if char in key.value:
+                return key
+
+        return StringCharType.OTHER
+
+    def consume(self, text: str) -> str:
+        for i, char in enumerate(text):
+            char_type = self._get_char_type(char)
+
+            match char_type:
+                case StringCharType.END:
+                    if self.state == StringMachineState.ESCAPE:
+                        self.state = StringMachineState.NORMAL
+                    else:
+                        self.done = True
+                        return text[i:]
+                case _:
+                    try:
+                        self.state = self.transitions[(self.state, char_type)]
+                    except KeyError:
+                        raise StateException
+
+        return ""
+
+    def get_valid_tokens(self, trie: Trie) -> set[int]:
+        if self.state == StringMachineState.ESCAPE:
+            return trie.constrained_search("\"\\/bfnrtu")
+        return set(range(trie.size))
+
+    def is_done(self) -> bool:
+        return self.done
 
 
 class StringRouterState(State):
@@ -164,7 +291,7 @@ class StringRouterState(State):
 
     def get_valid_tokens(self, trie: Trie) -> set[int]:
         return trie.search_batch_prefixes(
-            [state.string for state in self.options]
+            tuple([state.string for state in self.options])
         )
 
     def is_done(self) -> bool:
@@ -174,11 +301,20 @@ class StringRouterState(State):
         return self.next_states[self.winner]
 
 
-def get_string_router_automate(options: list[str]) -> StringRouterState:
+def get_string_router_automate(options: list[str], trie: Trie) -> StringRouterState:
     functions = [StaticStringState(option) for option in options]
     next_states = {}
 
     for function in functions:
-        next_states[function] = [StaticStringState("\",\"parameters\":{"), NumberState()]
+        next_states[function] = [StaticStringState("\",\"parameters\":{\"a\":"), NumberState()]
 
     return StringRouterState(set(functions), next_states)
+
+
+def get_boolean_automate() -> StringRouterState:
+    true_state = StaticStringState("true")
+    false_state = StaticStringState("false")
+
+    return StringRouterState(
+        {true_state, false_state}, {true_state: [], false_state: []}
+    )
